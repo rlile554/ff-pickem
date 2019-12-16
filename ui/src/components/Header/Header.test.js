80f242a1883b8f8ffa43784bc25ab40a1eb9c0c2
rlile554 <rlile554@gmail.com>
2019-12-15 21:37:40 -0500

    removed un-needed imports

--- ui/src/components/Header/Header.test.js
+++ ui/src/components/Header/Header.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 describe('Header', () => {
 
 
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render((<Router><Header /></Router>), div);
+        ReactDOM.render((<BrowserRouter><Header /></BrowserRouter>), div);
         ReactDOM.unmountComponentAtNode(div);
     });
 });
